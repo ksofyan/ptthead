246ca83cecbbb5387703bbfc457401af286c66fc
Karina Ramadhan <carinasofyan@gmail.com>
2019-03-18 18:38:28 +0800

    adding responsive to homepage

---
+++ assets/js/navtoggle.js
@@ -0,0 +1,9 @@
+var toggler = document.getElementById('nav-toggle');
+var navbar = document.getElementById('js-menu');
+toggler.addEventListener('click', () => {
+    if(js-menu.style.display === 'none') {
+        js-menu.style.display = 'block';
+    } else {
+        js-menu.style.display = 'none';
+    }
+});
